de31d25f7df0e8fdbb3abf61ee3ea1bad5d42b17
Pine Mizune <pinemz@gmail.com>
2016-02-21 22:15:28 +0900

    docs(rfc5988): fix docs

--- src/rfc5988.cr
+++ src/rfc5988.cr
@@ -9,10 +9,10 @@ require "./rfc5988/*"
 # there are HTTP response headers of GitHub API:
 #
 # ```
-# Link: &lt;https://api.github.com/search/code?q=addClass+user%3Amozilla&page=15>; rel="next",
-#   &lt;https://api.github.com/search/code?q=addClass+user%3Amozilla&page=34>; rel="last",
-#   &lt;https://api.github.com/search/code?q=addClass+user%3Amozilla&page=1&>; rel="first",
-#   &lt;https://api.github.com/search/code?q=addClass+user%3Amozilla&page=13>; rel="prev"
+# Link: <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=15>; rel="next",
+#   <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=34>; rel="last",
+#   <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=1&>; rel="first",
+#   <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=13>; rel="prev"
 # ```
 #
 # This library can parse a `Link` header value of RFC 5988, and also build it.
@@ -20,7 +20,7 @@ require "./rfc5988/*"
 #
 # ```crystal
 # require "rfc5988"
-# 
+#
 # values = RFC5988.parse("<http://www.example.com/next.html>; rel=\"next\"")
 # p values[0].url # => "http://www.example.com/next.html"
 # p values[0].rel # => "next"
@@ -30,7 +30,7 @@ require "./rfc5988/*"
 #
 # ```crystal
 # require "rfc5988"
-# 
+#
 # values = [LinkValue.new("http://www.example.com", {"rel" => "next"})]
 # p RFC5988.build(values) # => "<http://www.example.com>; rel=\"next\""
 # ```
